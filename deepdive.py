cbfd3d0ef9b4ce3e4cb983937ac7cdee1c7a0e56
Nandhini Prabha <sivaprakash@Nandhinis-MacBook-Air.local>
2025-01-31 14:51:18 +0530

    max_length

--- deepdive.py
+++ deepdive.py
@@ -24,7 +24,7 @@ def load_huggingface_model(prompt):
     model = AutoModelForCausalLM.from_pretrained(modelname)
     tokenizer = AutoTokenizer.from_pretrained(modelname)
     generator=pipeline('text-generation', model=model, tokenizer=tokenizer)
-    answer=generator(prompt, max_new_tokens=150, num_return_sequences=1)
+    answer=generator(prompt, max_new_tokens=150,truncation=True,max_length=1024,padding='max_length' ,num_return_sequences=1,return_tensors='pt')
     return answer
 
 
